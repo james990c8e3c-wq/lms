57529040c3d4346902562bbf8f93d3f75b173ac3
james990c8e3c-wq <james990c8e3c@uju.com.ng>
2026-01-03 04:03:47 +0000

    feat: Add assignPermissionsToRoles method to automatically sync all admin permissions to admin and sub_admin roles

--- Lernen/lernen-main-file/lernen/database/seeders/RolePermissionsSeeder.php
+++ Lernen/lernen-main-file/lernen/database/seeders/RolePermissionsSeeder.php
@@ -15,6 +15,7 @@ class RolePermissionsSeeder extends Seeder
     {
         $this->createRoles();
         $this->createPermissions();
+        $this->assignPermissionsToRoles();
     }
 
     private function createRoles(): void
@@ -70,6 +71,59 @@ class RolePermissionsSeeder extends Seeder
 
         foreach ($permissions as $permissionName) {
             $permission = Permission::firstOrCreate(['name' => $permissionName]);
+        }
+    }
+
+    private function assignPermissionsToRoles(): void
+    {
+        $permissions = [
+            'can-manage-courses',
+            'can-manage-badges',
+            'can-manage-course-bundles',
+            'can-manage-subscriptions',
+            'can-manage-forums',
+            'can-manage-insights',
+            'can-manage-menu',
+            'can-manage-option-builder',
+            'can-manage-pages',
+            'can-manage-email-settings',
+            'can-manage-notification-settings',
+            'can-manage-languages',
+            'can-manage-subjects',
+            'can-manage-subject-groups',
+            'can-manage-language-translations',
+            'can-manage-addons',
+            'can-manage-upgrade',
+            'can-manage-users',
+            'can-manage-identity-verification',
+            'can-manage-reviews',
+            'can-manage-invoices',
+            'can-manage-bookings',
+            'can-manage-withdraw-requests',
+            'can-manage-commission-settings',
+            'can-manage-payment-methods',
+            'can-manage-create-blogs',
+            'can-manage-all-blogs',
+            'can-manage-update-blogs',
+            'can-manage-blog-categories',
+            'can-manage-dispute',
+            'can-manage-disputes-list',
+            'can-manage-admin-users'
+        ];
+
+        // Get or create admin role
+        $adminRole = Role::where('name', 'admin')->first();
+        if ($adminRole) {
+            // Sync all permissions to admin role
+            $permissionIds = Permission::whereIn('name', $permissions)->pluck('id')->toArray();
+            $adminRole->permissions()->sync($permissionIds);
+        }
+
+        // Get or create sub_admin role and give all permissions
+        $subAdminRole = Role::where('name', 'sub_admin')->first();
+        if ($subAdminRole) {
+            $permissionIds = Permission::whereIn('name', $permissions)->pluck('id')->toArray();
+            $subAdminRole->permissions()->sync($permissionIds);
         }
     }
 }
